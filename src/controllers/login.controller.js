03ec232fc911e87068c72c7c2784ae5aa6a9018a
salimfenzar <s.fenzar@student.avans.nl>
2024-05-02 00:46:20 +0200

    added the module bcryptjs and deleted bcrypt

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken'); 
 const database = require('../../db/database');
 const assert = require('assert')
