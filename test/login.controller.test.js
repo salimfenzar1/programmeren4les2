e4ef3e38f8185d41e7ef1975fa292bfa6cd27cf9
salimfenzar <s.fenzar@student.avans.nl>
2024-05-05 23:03:19 +0200

    Added a test

--- test/login.controller.test.js
+++ test/login.controller.test.js
@@ -22,9 +22,6 @@ describe('UC-201 register /api/user',()=>{
                     emailAdress: 'v.a@server.nl'
                 })
                 .end((err, res) => {
-                    /**
-                     * Voorbeeld uitwerking met chai.expect
-                     */
                     chai.expect(res).to.have.status(400)
                     chai.expect(res).not.to.have.status(200)
                     chai.expect(res.body).to.be.a('object')
@@ -40,4 +37,24 @@ describe('UC-201 register /api/user',()=>{
                 })
         })
     });
+    describe('TC-201-2 Succesvol registreren', () => {
+        it('TC-201-2 Succesvol registreren', (done) => {
+            chai.request(server)
+                .post('/api/user')
+                .send({
+                    firstName: 'Voornaam',
+                    lastName: 'Achternaam',
+                    emailAddress: 'v.a@server.nl',
+                    password: 'wachtwoord123'
+                })
+                .end((err, res) => {
+                    chai.expect(res).to.have.status(201);
+                    chai.expect(res.body).to.be.a('object');
+                    chai.expect(res.body).to.have.property('message').equals('User registered successfully');
+                    chai.expect(res.body).to.have.property('user').that.includes({ firstName: 'Voornaam', lastName: 'Achternaam' });
+                    done();
+                });
+        });
+    });
 });
+
