606294752584576e1502c4aa856c4ed76973b2a9
salimfenzar <s.fenzar@student.avans.nl>
2024-05-10 00:45:58 +0200

    updated the test

--- test/login.controller.test.js
+++ test/login.controller.test.js
@@ -1,4 +1,10 @@
+
 process.env.DB_DATABASE = process.env.DB_DATABASE || 'share-a-meal-testdb';
+process.env.DB_PORT = 3306
+process.env.DB_USER = 'root'
+process.env.DB_PASSWORD = ''
+process.env.DB_HOST = 'localhost'
+
 process.env.LOGLEVEL = 'trace';
 
 const chai = require('chai');
