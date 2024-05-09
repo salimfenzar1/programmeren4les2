497b65f26c1cdfe08fe3ed2208c1f72811cc798e
salimfenzar1 <150351548+salimfenzar1@users.noreply.github.com>
2024-05-10 01:31:56 +0200

    Update login.controller.test.js

--- test/login.controller.test.js
+++ test/login.controller.test.js
@@ -1,9 +1,6 @@
 
 process.env.DB_DATABASE = process.env.DB_DATABASE || 'share-a-meal-testdb';
-process.env.DB_PORT = 3306
-process.env.DB_USER = 'root'
-process.env.DB_PASSWORD = ''
-process.env.DB_HOST = 'localhost'
+
 
 process.env.LOGLEVEL = 'trace';
 
